3f58e8a169bfe8332a61dcccb2a4d0f394eb83e0
Iris Tang <iriswht@gmail.com>
2016-04-02 16:47:47 -0700

    Add remove_edge to TweetGraph. Bug with VertexWithTS

--- src/average_degree.py
+++ src/average_degree.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 from datetime import datetime
 from dateutil import parser
+from decimal import Decimal, ROUND_DOWN
 import json
 import sys
 
@@ -14,50 +15,95 @@ class VertexWithTS(object):
 		self.ts = ts
 
 	def __eq__(self, other):
-		if not isinstance(other, Vertex):
+		if not isinstance(other, VertexWithTS):
 			return False
 
 		return self.name == other.name
 
+	def __ne__(self, other):
+		return not self.__eq__(other)
+		
 	def __hash__(self):
 		return hash(self.name)
 
 	def __str__(self):
 		return self.name #+ ',' + repr(self.ts)
 
+TWOPLACES = Decimal(10) ** -2
+
 class TweetGraph(object):
 	def __init__(self):
 		self._graph = {}
 		self._vertices = {}
 
-	def add_vertex_only(self, vertex, ts):
+	def add_vertex(self, vertex, ts):
 		if vertex not in self._vertices:
 			self._vertices[vertex] = ts
-			self._graph[vertex] = set()
+			self._graph[vertex] = [] #set() TODO(iristang): Figure out why remove() doesn't work
 		elif ts > self._vertices[vertex]:
 			self._vertices[vertex] = ts			
 
-	def _update_edge_set(self, vertex1, vertex2, ts):
-		toVertex = VertexWithTS(vertex2, ts)
-		if toVertex in self._graph[vertex1]:
-			oldTs = self._graph[vertex1].remove(toVertex).ts
-			if oldTs > toVertex.ts:
-				toVertex.ts = oldTs
+	def _update_edge_for_add(self, from_vertex, to_vertex, ts):
+		vertex_to_add = VertexWithTS(to_vertex, ts)
 
-		self._graph[vertex1].add(toVertex)
+		# If there is already an edge, check its TS is older; If not, we used its TS
+		if vertex_to_add in self._graph[from_vertex]:
+			index = self._graph[from_vertex].index(vertex_to_add)
+			oldTs = self._graph[from_vertex].pop(index).ts
+			#oldTs = self._graph[from_vertex].remove(vertex_to_add).ts
+			if oldTs > vertex_to_add.ts:
+				vertex_to_add.ts = oldTs
+
+		self._graph[from_vertex].append(vertex_to_add)
+		#self._graph[from_vertex].add(vertex_to_add)
 
 	def add_edge(self, vertex1, vertex2, ts):
-		self.add_vertex_only(vertex1, ts)
-		self.add_vertex_only(vertex2, ts)
+		self.add_vertex(vertex1, ts)
+		self.add_vertex(vertex2, ts)
 
-		self._update_edge_set(vertex1, vertex2, ts)
-		self._update_edge_set(vertex2, vertex1, ts)
+		self._update_edge_for_add(vertex1, vertex2, ts)
+		self._update_edge_for_add(vertex2, vertex1, ts)
+
+	def remove_vertex(self, vertex, ts):
+		assert(vertex in self._vertices)
+		if self._vertices[vertex] <= ts:
+			self._vertices.delete(vertex)
+			assert(len(self._graph[vertex]) == 0)
+			self._graph.delete(vertex)
+
+	def _update_edge_for_remove(self, from_vertex, to_vertex, ts):
+		vertex_to_remove = VertexWithTS(to_vertex, ts)
+		assert(vertex_to_remove in self._graph[from_vertex])
+		index = self._graph[from_vertex].index(vertex_to_remove)
+		current_vertex = self._graph[from_vertex].pop(index)
+		#current_vertex = self._graph[from_vertex].remove(vertex_to_remove)
+
+		# If there is a newer edge, we put it back
+		if current_vertex.ts > ts:
+			self._graph[from_vertex].add(current_vertex)
+
+	def remove_edge(self, vertex1, vertex2, ts):
+		self._update_edge_for_remove(vertex1, vertex2, ts)
+		self._update_edge_for_remove(vertex2, vertex1, ts)
+
+		self.remove_vertex(vertex1, ts)
+		self.remove_vertex(vertex2, ts)
 
 	def print_graph(self):
+		print(self.compute_avg_degree())
 		for k, v in self._graph.iteritems():
 			print(k, [str(e) for e in v])
 		for k, v in self._vertices.iteritems():
 			print(k, repr(v))
+
+	def compute_avg_degree(self):
+		degree_sum = 0
+		vertex_sum = 0
+		for v in self._graph.values():
+			vertex_sum += 1
+			degree_sum += len(v)
+
+		return Decimal(degree_sum*1.0/vertex_sum).quantize(TWOPLACES, rounding=ROUND_DOWN)
 
 class TweetLoader(object):
 	""" 
